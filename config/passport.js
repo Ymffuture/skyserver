b125a81aa079cfeee1736af8ef18578c23d22197
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-04-28 02:34:25 +0200

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,39 +1,39 @@
-
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import User from '../models/User.js';
-import dotenv from 'dotenv';
+// config/passport.js
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import dotenv from "dotenv";
+import User from "../models/User.js";
 
 dotenv.config();
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ googleId: profile.id });
-
-    if (!user) {
-      user = new User({
-        googleId: profile.id,
-        displayName: profile.displayName,
-        email: profile.emails[0].value,
-        avatar: profile.photos[0].value,
-      });
-      await user.save();
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/api/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            displayName: profile.displayName,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value,
+          });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
     }
+  )
+);
 
-    done(null, user);
-  } catch (err) {
-    done(err, null);
-  }
-}));
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
+// Serialize & deserialize
+passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
@@ -42,3 +42,6 @@ passport.deserializeUser(async (id, done) => {
     done(err, null);
   }
 });
+
+// Export the configured passport instance as default
+export default passport;
